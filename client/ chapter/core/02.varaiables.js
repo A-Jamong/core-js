698a0423e7ef3bded8c0268d7d2e3dc0dd122eb4
JAM <99.1.some@gmail.com>
2024-05-31 11:09:21 +0900

    실습

---
+++ client/ chapter/core/02.varaiables.js
@@ -0,0 +1,34 @@
+/* ------------------ */
+/* Variables          */
+/* ------------------ */
+
+let admin;
+let name="John";
+admin=name;
+alert(admin);
+
+let ourPlanet = "지구";
+let onlineUserName;
+
+
+/* 다음 내용을 분석한 후, 프로그래밍 할 수 있도록 변수와 상수를 작성해봅니다. ----------- */
+
+// - 갯수 별 상품 가격 계산하기
+let priceByNumber
+// - 구매 제품 가격의 총 합
+let totalPriceofProduct
+// - 1년 기준 일(day)자 수
+const DAY_1YEAR
+// - 구매 결제 여부
+let buyWhether /*-> isPayment hasCartList is has*/
+// - 구매 결제 내역
+let listBuy
+// - 브랜드 접두사
+const brand 
+// - 오늘의 운세
+let todayLuck
+// - 상품 정보
+const PRODUCT_INFO
+/* variables ----------------------------------------------------------- */
+
+/* constant variables -------------------------------------------------- */
